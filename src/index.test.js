e864fc67f3751b3b84bf2f146c1577d270b55670
sheygs13 <segun.ekoh@gmail.com>
2020-05-07 14:22:33 +0100

    :white_check_mark: update tests

--- src/index.test.js
+++ src/index.test.js
@@ -1,8 +1,29 @@
 const coolStarWars = require('./index');
 const expect = require('chai').expect;
 
-describe('StarWars - names', function(){
-   it('should work!', function(){
-      expect(true).to.be.true;
-   });
+describe('StarWars names', function(){
+  describe('all', function(){
+     it('should be an array', function(){
+       expect(coolStarWars.all).to.be.an('array');
+     });
+
+     it('should be an array of string', function(){
+       expect(coolStarWars.all).to.satisfy(arrayOfStrings);
+
+       function arrayOfStrings(arr){
+         return arr.every(el => typeof el === 'string');
+       }
+     })
+
+     it('should contain Sheygs Mcpherson', function(){
+        expect(coolStarWars.all).to.contain('Sheygs Mcpherson')
+     })
+  })
+   
+  describe('random', function(){
+    it('should return a random starwars name', function(){
+      const randomStarName = coolStarWars.random();
+      expect(coolStarWars.all).to.include(randomStarName);
+    })
+  })
 });
