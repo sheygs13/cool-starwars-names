21e050004755439151d5ee771b326f918b0e40de
sheygs13 <segun.ekoh@gmail.com>
2020-05-07 13:26:45 +0100

    :white_check_mark: set up unit testing with mocha and chai

---
+++ src/index.test.js
@@ -0,0 +1,8 @@
+const coolStarWars = require('./index');
+const expect = require('chai').expect;
+
+describe('StarWars - names', function(){
+   it('should work!', function(){
+      expect(true).to.be.true;
+   });
+});
